1305c39856ee8bf1c4a18695a0dec27b479ee210
ehsan <51818809+sehsanali@users.noreply.github.com>
2023-09-10 15:50:31 +0200

    Quartz sync: Sep 10, 2023, 3:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "ehsan.xyz",
+    baseUrl: "sehsanali.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
